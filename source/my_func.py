9e0d8df64de2076b6a4d7c5f29203f86b97392b2
adib-the-noob <mohammed.adib1104@gmail.com>
2025-07-06 23:48:42 +0600

    all code added

--- source/my_func.py
+++ source/my_func.py
@@ -3,3 +3,8 @@ def add(number_one, number_two):
 
 def divide(number_one, number_two):
     return number_one / number_two
+
+def divide_by_zero(number_one, number_two):
+    if number_two == 0:
+        raise ValueError("You cannot divide by zero")
+    return number_one / number_two
--- tests/test_my_func.py
+++ tests/test_my_func.py
@@ -8,3 +8,11 @@ def test_add():
 def test_divide():
     result = my_func.divide(10, 2)
     assert result == 5, f"Expected 5, but got {result}"
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        result = my_func.divide(10, 0)
+
+def test_divide_by_zero_2():
+    with pytest.raises(ValueError):
+        result = my_func.divide_by_zero(10, 0)
